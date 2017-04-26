428667de46c073239a4253bfd2e1d101291e2f09
Mei Lin Antazo <meilinantazo@LT375.local>
2017-04-27 05:06:49 +0800

    Dashboard

--- app/models/chart.js
+++ app/models/chart.js
@@ -2,65 +2,9 @@ import DS from 'ember-data';
 import Ember from 'ember';
 
 export default DS.Model.extend({
-  firstName: DS.attr(),
-  lastName: DS.attr(),
-  photo: DS.attr(),
-  position: DS.attr(),
-  employeeId: DS.attr(),
-
-  subordinates: DS.hasMany('user', { inverse: 'superior' }),
-  superior: DS.belongsTo('user', { inverse: 'subordinates' }),
-  evaluation: DS.belongsTo('evaluation', { inverse: 'owner' }),
-  activities: DS.hasMany('activity', { inverse: 'owner' }),
-  notifications: DS.hasMany('notification', { inverse: 'owner' }),
-  topics: DS.hasMany('notification', { inverse: 'subject' }),
-
-  talents: DS.hasMany('user', { inverse: 'talentManager' }),
-  talentManager: DS.belongsTo('user', { inverse: 'talents' }),
-
-  showInWarning: false,
-
-  isTalentManager: Ember.computed.bool('talents.length'),
-
-  hasWarningSubordinates: Ember.computed('subordinates.@each.showInWarning', 'subordinates.@each.hasWarningSubordinates', function() {
-    let hasWarningSubs = false;
-    let subs = this.get('subordinates');
-    subs.forEach(sub => {
-      if (sub.get('hasWarningSubordinates') || sub.get('showInWarning')) {
-        hasWarningSubs = true;
-      }
-    });
-    return hasWarningSubs;
-  }),
-
-  warningSubordinates: Ember.computed('subordinates.@each.showInWarning', 'subordinates.@each.warningSubordinates', function() {
-    let warningSubs = [];
-    let subs = this.get('subordinates');
-    subs.forEach(sub => {
-      warningSubs.addObjects(sub.get('warningSubordinates'));
-      if (sub.get('showInWarning')) {
-        warningSubs.addObject(sub);
-      }
-    });
-    return warningSubs;
-  }),
-
-  fullName: Ember.computed('firstName', 'lastName', function() {
-    return `${this.get('lastName')}, ${this.get('firstName')}`;
-  }),
-
-  lastUpdated: Ember.computed('subordinates', function() {
-    let lastUpdated = null;
-    let subs = this.get('subordinates');
-    subs.forEach(sub => {
-      if(lastUpdated == null || lastUpdated < sub.get('evaluation.lastUpdated')) {
-        lastUpdated = sub.get('evaluation.lastUpdated');
-      }
-    });
-    return lastUpdated;
-  }),
-
-  chartData: Ember.computed('subordinates', function() {
+  owner: DS.belongsTo('user', { inverse: 'chart' }),
+  
+  chartData: Ember.computed('subordinates.@each.evaluation', function() {
     let rated1 = 0;
     let rated2 = 0;
     let rated3 = 0;
