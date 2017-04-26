28140a604dcb2caa27b80478be8a4f0853247f8c
John Rema de Guzman <deguzmanjohnrema@gmail.com>
2017-04-27 02:36:59 +0800

    completed notifications page

--- app/models/evaluation.js
+++ app/models/evaluation.js
@@ -3,7 +3,6 @@ import Ember from 'ember'
 
 export default DS.Model.extend({
   owner: DS.belongsTo('user', { inverse: 'evaluation' }),
-  showInWarning: DS.attr(),
   lastUpdated: DS.attr('date'),
   rating: DS.attr(),
   parameter: DS.attr(),
--- app/models/notification.js
+++ app/models/notification.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   owner: DS.belongsTo('user', { inverse: 'notifications' }),
-  subject: DS.belongsTo('user', { inverse: 'topics' })
+  subject: DS.belongsTo('user', { inverse: 'topics' }),
+  isOpen: false
 });
