d98cb862c74159f03231a45c21498a5959bcb9ef
John Rema de Guzman <deguzmanjohnrema@gmail.com>
2017-04-27 02:42:40 +0800

    cleanup

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,26 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    var data = {
-      datasets: [{
-          data: [
-              11,
-              16,
-              7,
-              3,
-              14
-          ],
-          backgroundColor: [
-              "#FF6384",
-              "#4BC0C0",
-              "#FFCE56",
-              "#E7E9ED",
-              "#36A2EB"
-          ],
-          label: 'My dataset' // for legend
-      }]
-    }
-    return data;
-  }
 });
