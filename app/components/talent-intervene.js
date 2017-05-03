ef83f28600facc96fbac4492b15cb01dfb3739c9
Mei Lin Antazo <meilinantazo@LT375.local>
2017-05-03 23:02:50 +0800

    Modal

--- app/components/talent-intervene.js
+++ app/components/talent-intervene.js
@@ -2,9 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isShowingModal: false,
+  isShowingAddContact: false,
+
+  
+
   actions: {
     toggleModal: function() {
       this.toggleProperty('isShowingModal');
+    },
+    toggleAddContact: function() {
+      this.toggleProperty('isShowingAddContact');
     }
   }
 });
