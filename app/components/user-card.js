77e6ac696f19b4afc09a9e5e0b3a2dcf2ffc0d2d
Mei Lin Antazo <meilinantazo@LT375.local>
2017-05-10 23:16:15 +0800

    Sign out link

--- app/components/user-card.js
+++ app/components/user-card.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   photo: null,
   name: null,
-  position: null
+  position: null,
+  dropdown: false,
+
+  actions: {
+    toggleDropdown() {
+      this.toggleProperty('dropdown');
+    }
+  }
 });
