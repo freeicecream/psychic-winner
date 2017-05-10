332d09ec28d99bb4a3058186cc67cbadd133a6ef
John Rema de Guzman <deguzmanjohnrema@gmail.com>
2017-05-10 23:56:06 +0800

    authenticated routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
   beforeModel() {
     this._super(...arguments);
     this.replaceWith('home');
--- app/routes/logout.js
+++ app/routes/logout.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
   session: Ember.inject.service(),
   beforeModel() {
     this._super(...arguments);
     this.get('session').invalidate();
-    this.transitionTo('login');
   }
 });
